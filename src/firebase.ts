66307f3b2d678dfefc7e71e22d894873cc5d4c6a
jeminsieow <sjemin7@gmail.com>
2021-09-22 00:26:25 +0800

    Add basic push notification functionality

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getAuth } from "@firebase/auth";
+import { getMessaging, getToken } from "@firebase/messaging";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,4 +18,30 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+
 export const auth = getAuth();
+
+const messaging = getMessaging();
+getToken(messaging, {
+  vapidKey:
+    "BHPkVosHmRtZQ6gEXYws3DnbtXw-MTngfNYUPgsgF209IETuF-wWOGGXr_VdPlVgeDrbmT1dW8DEk6qSYrEWF4g",
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+      console.log('here is token')
+      console.log(currentToken);
+    } else {
+      // Show permission request UI
+      console.log(
+        "No registration token available. Request permission to generate one."
+      );
+      // ...
+    }
+  })
+  .catch((err) => {
+    console.log("An error occurred while retrieving token. ", err);
+    // ...
+  });
+
